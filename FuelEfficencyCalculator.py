b176121ec4ba125fabea6feacfcedae39293c198
Johnest2 <h_stoppel@web.de>
2022-05-22 23:15:45 +0200

    started initial implementaion of gui and added calculation for given values. not working state

--- FuelEfficencyCalculator.py
+++ FuelEfficencyCalculator.py
@@ -5,7 +5,9 @@ import os
 import sys
 from pathlib import Path
 
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
+
+from gui.mainWindow import MainWindow
 
 
 def main(args):
@@ -14,14 +16,14 @@ def main(args):
 
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-    appDataDir = Path(os.getenv("APPDATA")).joinpath("phoneTimer")
+    appDataDir = Path(os.getenv("APPDATA")).joinpath("FuelEfficencyCalculator")
     if not appDataDir.exists():
         appDataDir.mkdir
 
-    # Write main code here!
+    mainWindow=MainWindow()
 
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
